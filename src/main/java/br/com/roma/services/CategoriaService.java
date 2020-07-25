187cc13f0c1667bc168bf4f2cbca8566176b8e9f
Henrique Roma <hrlima7@gmail.com>
2020-07-25 18:29:05 -0300

    criacao repository e  service Categoria

---
+++ src/main/java/br/com/roma/services/CategoriaService.java
@@ -0,0 +1,26 @@
+package br.com.roma.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.roma.domain.Categoria;
+import br.com.roma.repository.CategoriaRepository;
+
+
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	
+	public Categoria buscar(Integer id) {
+		Optional <Categoria> obj = repo.findById(id);
+	
+		return obj.orElse(null);
+	}
+
+}
