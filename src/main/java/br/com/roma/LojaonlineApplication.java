b3d2252a7dec224d9ab84ac70a58af436ed28b56
Henrique Roma <hrlima7@gmail.com>
2020-07-25 19:18:39 -0300

    produtos e categorias muitos para muitos

--- src/main/java/br/com/roma/LojaonlineApplication.java
+++ src/main/java/br/com/roma/LojaonlineApplication.java
@@ -1,13 +1,22 @@
 package br.com.roma;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LojaonlineApplication {
+import br.com.roma.domain.Categoria;
+import br.com.roma.repository.CategoriaRepository;
 
+@SpringBootApplication
+public class LojaonlineApplication{
+
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LojaonlineApplication.class, args);
 	}
 
+	
 }
