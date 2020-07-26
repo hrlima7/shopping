c70e4817831f9191449f473b69e8ff6e9173b1e3
Henrique Roma <hrlima7@gmail.com>
2020-07-26 19:47:40 -0300

    Mapeamento relacionanal das classes Pagamento e pedido

--- src/main/java/br/com/roma/domain/Pedido.java
+++ src/main/java/br/com/roma/domain/Pedido.java
@@ -1,12 +1,25 @@
 package br.com.roma.domain;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Pedido {
-	
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Pedido implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private Date instante;
 
+	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
 	
 	private Cliente cliente;
@@ -65,6 +78,32 @@ public class Pedido {
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pedido other = (Pedido) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
 	
 	
 	
